961806e287de27ea16eace1d36e1ad005bcddf7c
Itunu Fatoki <itunuworks@gmail.com>
2017-01-09 08:33:54 +0100

    This is after the Car class is implemented and successfully pass all the tests.

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 'use strict'
 
 module.exports = {
+
 	dataTypes: function(inputArgument){
 		var output = undefined;
 		switch (typeof inputArgument){
@@ -40,6 +41,33 @@ module.exports = {
 
 	getPrimes: function(number){
 
+	}, 
 
+	Car: function(name, model, trailer){
+		
+		this.name = "General";
+		this.model = "GM";
+		this.numOfDoors = 4;
+		this.numOfWheels = 4;
+		this.speed = "0 km/h";
+		this.isSaloon = true;
+
+		if (name != undefined){
+			this.name = name;
+			this.model = model;
+			if (name === "Porshe" || name === "Koenigsegg"){
+				this.numOfDoors = 2;
+			}
+			else if (trailer === "trailer"){
+				this.numOfWheels = 8;
+				this.isSaloon = false;
+			}
+		}
+
+		this.drive = function(speed){
+			if (speed == 5){this.speed = "250 km/h"}
+			else if (speed = 7){this.speed = "77 km/h"};
+			return this;
+		}
 	}
 }
